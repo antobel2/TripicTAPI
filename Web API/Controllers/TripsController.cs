60a7fc0b5a19f3c0c2dfb1b0544f7096f0cbec3d
1028406 <1028406@LLBD0605INFO06>
2019-01-28 14:50:26 -0500

    t

---
+++ Web API/Controllers/TripsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    public class TripsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Trips
+        public IQueryable<Trip> GetTrips()
+        {
+            return db.Trips;
+        }
+
+        // GET: api/Trips/5
+        [ResponseType(typeof(Trip))]
+        public IHttpActionResult GetTrip(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(trip);
+        }
+
+        // Post: api/CreateTrip
+        [Route("api/Trip/CreateTrip")]
+        [HttpPost]
+        public IHttpActionResult CreateTrip(Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Error in the model of request api/Trip/CreateTrip");
+            }
+
+            var tripResults = db.Trips.ToList();
+            foreach (Trip i in tripResults)
+            {
+                if (i.Name == trip.Name)
+                {
+                    trip.Name = trip.Name + " (" + DateTime.Now + ")";
+                }
+            }
+            db.Trips.Add(trip);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        // PUT: api/Trips/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTrip(int id, Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != trip.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(trip).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Trips
+        [ResponseType(typeof(Trip))]
+        public IHttpActionResult PostTrip(Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Trips.Add(trip);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = trip.Id }, trip);
+        }
+
+        // DELETE: api/Trips/5
+        [ResponseType(typeof(Trip))]
+        public IHttpActionResult DeleteTrip(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+
+            return Ok(trip);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TripExists(int id)
+        {
+            return db.Trips.Count(e => e.Id == id) > 0;
+        }
+    }
+}
