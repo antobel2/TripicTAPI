baf0fbb4911bd5876694e40c4bb07b260127a952
1507779 <1507779@LLBD0605INFO07>
2019-02-22 14:58:31 -0500

    get and create comments works

--- WebAPI.Tests/TripControllerTest.cs
+++ WebAPI.Tests/TripControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Security.Principal;
@@ -22,14 +23,10 @@ namespace WebAPI.Tests
         {
             _controller = new TripsController();
         }
-        [TestMethod]
-        public void InviteUserToTrip_GoodValues_OK()
+
+        [TestInitialize]
+        private void initializeTestMethod()
         {
-            var testCreateTripDTO = new CreateTripDTO()
-            {
-                Name = "Test à cuba"
-            };
-            
             var config = new HttpConfiguration();
             var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost/api/Trips/InviteUserToTrip");
             var route = config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}");
@@ -38,9 +35,102 @@ namespace WebAPI.Tests
             _controller.ControllerContext = new HttpControllerContext(config, routeData, request);
             _controller.Request = request;
             _controller.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = config;
-            var response = _controller.CreateTrip(testCreateTripDTO);
-
-            Assert.AreEqual(request.CreateResponse(HttpStatusCode.OK).StatusCode, response.StatusCode);
         }
+
+        [TestMethod]
+        public void InviteUserToTrip_GoodValues_OK()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                TripId = 1,
+                UserIds = new List<string>()
+                {
+                    "04b6dea7-fd92-43ae-9912-f563fbff42d8"
+                }
+            };
+            
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.OK).StatusCode, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void InviteUserToTrip_NoTripId_KO()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                UserIds = new List<string>()
+                {
+                    "04b6dea7-fd92-43ae-9912-f563fbff42d8"
+                }
+            };
+            
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.BadRequest, "Le model InviteUserToTripDTO n'est pas valide").ReasonPhrase, response.ReasonPhrase);
+        }
+
+        [TestMethod]
+        public void InviteUserToTrip_NoUserIds_KO()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                TripId = 1,
+                UserIds = new List<string>()
+                {
+                    
+                }
+            };
+
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.BadRequest, "Le model InviteUserToTripDTO n'est pas valide").ReasonPhrase, response.ReasonPhrase);
+        }
+
+        [TestMethod]
+        public void InviteUserToTrip_TripNotFound_KO()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                TripId = int.MaxValue,
+                UserIds = new List<string>()
+                {
+                    "04b6dea7-fd92-43ae-9912-f563fbff42d8"
+                }
+            };
+            
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.NotFound, "L'id du voyage n'a retourné aucun résultats").ReasonPhrase, response.ReasonPhrase);
+        }
+
+        [TestMethod]
+        public void InviteUserToTrip_UserNotPermitted_KO()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                TripId = int.MaxValue,
+                UserIds = new List<string>()
+                {
+                    "04b6dea7-fd92-43ae-9912-f563fbff42d8"
+                }
+            };
+            
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.Forbidden, "L'utilisateur actuel n'a pas les droits d'accès au voyage demandé").ReasonPhrase, response.ReasonPhrase);
+        }
+
+        [TestMethod]
+        public void InviteUserToTrip_UserToAddNotFound_KO()
+        {
+            InviteUserToTripDTO model = new InviteUserToTripDTO()
+            {
+                TripId = int.MaxValue,
+                UserIds = new List<string>()
+                {
+                    "notValidId"
+                }
+            };
+            
+            var response = _controller.InviteUserToTrip(model);
+            Assert.AreEqual(_controller.Request.CreateResponse(HttpStatusCode.NotFound, "L'id de l'utilisateur à inviter n'a retourné aucun résultats").ReasonPhrase, response.ReasonPhrase);
+        }
+        
     }
 }
